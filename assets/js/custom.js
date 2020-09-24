5f5024fca4d81aabd3229ef8db2e7e98d00bc6c0
varvino <graafmans.tom@gmail.com>
2020-09-24 15:05:33 -0400

    Custom JS: removed unused scripts

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,35 +12,6 @@ function closeNav() {
 }
 "use strict";
 
-var slider = document.querySelector('.items');
-var isDown = false;
-var startX;
-var scrollLeft;
-slider.addEventListener('mousedown', function (e) {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener('mouseleave', function () {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mouseup', function () {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mousemove', function (e) {
-  if (!isDown) return;
-  e.preventDefault();
-  var x = e.pageX - slider.offsetLeft;
-  var walk = (x - startX) * 3; //scroll-fast
-
-  slider.scrollLeft = scrollLeft - walk;
-  console.log(walk);
-});
-"use strict";
-
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 var vh = window.innerHeight * 0.01; // Then we set the value in the --vh custom property to the root of the document
 
